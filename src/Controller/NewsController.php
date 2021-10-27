1d34f20ab7cc98765f4bb89fa8333b8c56ecd2e4
Gabsy <gfournier-neri@laposte.net>
2021-10-27 15:29:15 +0200

    commit break

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
 
 class NewsController extends AbstractController
 {
@@ -17,6 +18,27 @@ class NewsController extends AbstractController
             'news' => 'Hello News'
         ]);
     }
+
+
+    /**
+     * @Route("/finder", name="finder")
+     */
+    public function customFinder(): Response
+    {
+        $finder = new Finder();
+
+        $finder->files()->in(__DIR__);
+
+        foreach ($finder as $file) {
+            echo $file ."<br/>";
+            echo $file->getRealPath() ."<br/>";
+            echo $file->getRelativePathname() ."<br/>";
+        }
+        return new Response();
+    }
+
+
+
 }
 
 
